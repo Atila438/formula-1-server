0c8b14d1b652387442fef128ac56e818641e5e2c
Atila438 <104580581+Atila438@users.noreply.github.com>
2025-08-11 13:00:49 +0300

    sixth commit

--- routes/driver_routes.py
+++ routes/driver_routes.py
@@ -1,11 +1,11 @@
 # routes/driver_routes.py
 from flask import Blueprint
 import controllers.driver_controller as driver_controller
+
 driver_bp = Blueprint("driver_bp", __name__)
 
 driver_bp.route("/", methods=["GET"])(driver_controller.home)
-# Home route for driver-related endpoints
-driver_bp.route("/", methods=["GET"])(driver_controller.home)
+driver_bp.route("/driver/<name>", methods=["GET"])(driver_controller.fetch_driver)
 driver_bp.route("/drivers", methods=["GET"])(driver_controller.fetch_all_drivers)
 driver_bp.route("/driver", methods=["POST"])(driver_controller.create_driver)
 driver_bp.route("/driver/<name>/points", methods=["PATCH"])(driver_controller.add_points)
